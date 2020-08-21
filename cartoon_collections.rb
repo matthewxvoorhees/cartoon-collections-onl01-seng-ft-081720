9229ce2c708c214483fe2e55dadbc584df634958
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-21 19:46:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(summon)
-  
-  names.map! {|name| name.capitalize}
+summon.map do {|name| name.capitalize}
+end
 end
 
 def long_planeteer_calls# code an argument here
