5baa5708ad8d8ad15bcbd4a45b7bae0f08cb7b99
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-21 20:09:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(summon)
+array = []
 summon.map do |name|
-  return "#{name.capitalize}!"
+  array << "#{name.capitalize}!"
   end
 end
 
